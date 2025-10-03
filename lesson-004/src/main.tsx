b4cbfb70e271b0c37b8051a3ba593a3186a4ccb1
Alty <heneveraskedforthis@gmail.com>
2025-10-03 23:19:22 +0300

    lessons 001-004

---
+++ lesson-004/src/main.tsx
@@ -0,0 +1,15 @@
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+/*В TypeScript знак "!" означает, что мы уверены, что какой-то элемент не равен null или undefined, даже если TypeScript
+думает, что он может быть null.*/
+const rootEl = document.getElementById('root')!;
+const reactRootEl = createRoot(rootEl);
+
+reactRootEl.render(
+    <StrictMode>
+        <App/>
+    </StrictMode>,
+)
