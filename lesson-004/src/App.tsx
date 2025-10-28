ac90cee06fe1f9ca698d6a526775b2b48daa41a3
Alty <heneveraskedforthis@gmail.com>
2025-10-28 20:41:09 +0300

    lesson 009 useState

--- lesson-004/src/App.tsx
+++ lesson-004/src/App.tsx
@@ -1,10 +1,11 @@
+import {useState} from 'react';
+
 const tracks = [
     {
         id: 1,
         title: 'Musicfun soundtrack',
         url: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3'
     },
-
     {
         id: 2,
         title: 'Musicfun soundtrack instrumental',
@@ -12,30 +13,9 @@ const tracks = [
     }
 ];
 
-const track01El = <li>
-    <div>{tracks[0].title}</div>
-    <audio src={tracks[0].url} controls></audio>
-</li>;
-
-const track02El = <li>
-    <div>{tracks[1].title}</div>
-    <audio src={tracks[1].url} controls></audio>
-</li>;
-
-const tracks01Els = [track01El, track02El];
-
-const tracks02Els = tracks.map((track, index) => {
-    return (
-        <li key={index}>
-            <div>{track.title}</div>
-            <audio src={track.url} controls></audio>
-        </li>
-    );
-});
-
-const selectedTrackId = 1;
-
 export function App() {
+    const [selectedTrackId, setSelectedTrackId] = useState(null);
+
     if (!tracks) {
         return <div>
             <h1>Musicfun</h1>
@@ -53,67 +33,7 @@ export function App() {
     return (
         <div>
             <h1>Musicfun Player</h1>
-
-            {/*<ul>*/}
-            {/*    <h3>001</h3>*/}
-
-            {/*    <li>*/}
-            {/*        <div>Musicfun soundtrack</div>*/}
-            {/*        <audio src="https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3" controls></audio>*/}
-            {/*    </li>*/}
-
-            {/*    <li>*/}
-            {/*        <div>Musicfun soundtrack instrumental</div>*/}
-            {/*        <audio src="https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3"*/}
-            {/*               controls></audio>*/}
-            {/*    </li>*/}
-
-            {/*    <hr></hr>*/}
-            {/*    <h3>002</h3>*/}
-
-            {/*    <li>*/}
-            {/*        <div>{tracks[0].title}</div>*/}
-            {/*        <audio src={tracks[0].url} controls></audio>*/}
-            {/*    </li>*/}
-
-            {/*    <li>*/}
-            {/*        <div>{tracks[1].title}</div>*/}
-            {/*        <audio src={tracks[1].url} controls></audio>*/}
-            {/*    </li>*/}
-
-            {/*    <hr></hr>*/}
-            {/*    <h3>003</h3>*/}
-
-            {/*    {track01El}*/}
-            {/*    {track02El}*/}
-
-            {/*    <hr></hr>*/}
-            {/*    <h3>004</h3>*/}
-
-            {/*    /!*React может распарсить массив и отрисовать его элементы сам автоматически.*!/*/}
-            {/*    {tracks01Els}*/}
-
-            {/*    <hr></hr>*/}
-            {/*    <h3>005</h3>*/}
-
-            {/*    {tracks02Els}*/}
-
-            {/*    <hr></hr>*/}
-            {/*    <h3>006</h3>*/}
-
-            {/*    {*/}
-            {/*        tracks.map(*/}
-            {/*            (track, index) => {*/}
-            {/*                return (*/}
-            {/*                    <li key={index}>*/}
-            {/*                        <div>{track.title}</div>*/}
-            {/*                        <audio src={track.url} controls></audio>*/}
-            {/*                    </li>*/}
-            {/*                );*/}
-            {/*            }*/}
-            {/*        )*/}
-            {/*    }*/}
-            {/*</ul>*/}
+            <button onClick={() => setSelectedTrackId(null)}>Reset selection</button>
 
             <ul>
                 {tracks.map((track) => {
@@ -121,7 +41,9 @@ export function App() {
                         <li key={track.id}
                             style={{border: track.id === selectedTrackId ? '1px solid orange' : 'none'}}
                         >
-                            <div>{track.title}</div>
+                            <div onClick={() => { setSelectedTrackId(track.id); }}>
+                                {track.title}
+                            </div>
                             <audio src={track.url} controls></audio>
                         </li>
                     );
