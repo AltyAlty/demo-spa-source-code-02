ab8ebb316eb0c94659efd782288c597652811333
Alty <heneveraskedforthis@gmail.com>
2025-11-03 18:03:55 +0300

    lesson 013 track details

--- lesson-004/src/App.tsx
+++ lesson-004/src/App.tsx
@@ -3,12 +3,11 @@ import {useState, useEffect} from 'react';
 export function App() {
     const [tracks, setTracks] = useState(null);
     const [selectedTrackId, setSelectedTrackId] = useState(null);
+    const [selectedTrack, setSelectedTrack] = useState(null);
 
     useEffect(() => {
         fetch(`${import.meta.env.VITE_BASE_URL}playlists/tracks`, {
-            headers: {
-                'api-key': import.meta.env.VITE_API_KEY,
-            }
+            headers: {'api-key': import.meta.env.VITE_API_KEY}
         })
             .then(res => res.json())
             .then(json => setTracks(json.data));
@@ -31,22 +30,53 @@ export function App() {
     return (
         <div>
             <h1>Musicfun Player</h1>
-            <button onClick={() => setSelectedTrackId(null)}>Reset selection</button>
+            <button onClick={
+                () => {
+                    setSelectedTrackId(null);
+                    setSelectedTrack(null);
+                }
+            }>
+                Reset selection
+            </button>
 
-            <ul>
-                {tracks.map((track) => {
-                    return (
-                        <li key={track.id}
-                            style={{border: track.id === selectedTrackId ? '1px solid orange' : 'none'}}
-                        >
-                            <div onClick={() => { setSelectedTrackId(track.id); }}>
-                                {track.attributes.title}
-                            </div>
-                            <audio src={track.attributes.attachments[0].url} controls></audio>
-                        </li>
-                    );
-                })}
-            </ul>
+            <div style={{display: 'flex', gap: '30px'}}>
+                <ul>
+                    {tracks.map((track) => {
+                        return (
+                            <li key={track.id}
+                                style={{border: track.id === selectedTrackId ? '1px solid orange' : 'none'}}
+                            >
+                                <div onClick={() => {
+                                    setSelectedTrackId(track.id);
+
+                                    fetch(`${import.meta.env.VITE_BASE_URL}playlists/tracks/${track.id}`, {
+                                        headers: {'api-key': import.meta.env.VITE_API_KEY}
+                                    })
+                                        .then(res => res.json())
+                                        .then(json => setSelectedTrack(json.data));
+                                }}>
+                                    {track.attributes.title}
+                                </div>
+
+                                <audio src={track.attributes.attachments[0].url} controls></audio>
+                            </li>
+                        );
+                    })}
+                </ul>
+
+                <div>
+                    <h3>Details</h3>
+
+                    {!selectedTrack && !selectedTrackId && 'No track selected'}
+                    {!selectedTrack && selectedTrackId && 'Loading...'}
+                    {selectedTrack && (
+                        <div>
+                            <h3>{selectedTrack.attributes.title}</h3>
+                            <p>{selectedTrack.attributes.lyrics ?? 'no lyrics'}</p>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
     );
 };
